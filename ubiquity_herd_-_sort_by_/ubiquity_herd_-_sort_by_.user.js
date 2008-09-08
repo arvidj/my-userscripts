237e8882ee4c39a16915d074c95bdf9deedc237c
Arvid Jakobsson <arvid.jakobsson@gmail.com>
2008-09-08 19:02:49 +0200

    reindented

--- ubiquity_herd_-_sort_by_/ubiquity_herd_-_sort_by_.user.js
+++ ubiquity_herd_-_sort_by_/ubiquity_herd_-_sort_by_.user.js
@@ -6,178 +6,203 @@
 // ==/UserScript==
 
 var herdSorter = {
-	volatile: {
-		rowxp: "//tr[position() > 1]",
-		cellxp: ".//td[5]"
-	},
+  unstable: {
+    rowxp: "//tr[position() > 1]",
+    cellxp: ".//td[5]"
+  },
 
-	headers: {
-		url: { 
-			xp: "//tr[1]//th[1]",
-			cell_xp: ".//td[1]",
-			sort: sortstr
-		},
-		birth: { 
-			xp: "//tr[1]//th[2]",
-			cell_xp: ".//td[2]",
-			sort: dateSort
-		},
-		lastseen: { 
-			xp: "//tr[1]//th[3]",
-			cell_xp: ".//td[3]",
-			sort: dateSort,
-			sortby: true
-		},
-		lifespan: { 
-			xp: "//tr[1]//th[4]",
-			cell_xp: ".//td[4]",
-			sort: dateSort
-		},
-		subscribers: { 
-			xp: "//tr[1]//th[5]",
-			cell_xp: ".//td[5]",
-			sort: sortf
-		},
-	},
+  headers: {
+    url: {
+      xp: "//tr[1]//th[1]",
+      cell_xp: ".//td[1]",
+      sort: sortstr
+    },
+    birth: {
+      xp: "//tr[1]//th[2]",
+      cell_xp: ".//td[2]",
+      sort: dateSort
+    },
+    lastseen: {
+      xp: "//tr[1]//th[3]",
+      cell_xp: ".//td[3]",
+      sort: dateSort,
+      sortby: true
+    },
+    lifespan: {
+      xp: "//tr[1]//th[4]",
+      cell_xp: ".//td[4]",
+      sort: dateSort
+    },
+    subscribers: {
+      xp: "//tr[1]//th[5]",
+      cell_xp: ".//td[5]",
+      sort: sortf
+    }
+  },
 
-	setListeners: function() {
-		this.rows = $x(this.volatile.rowxp);
-		this.rows.reinsert = function () { this.forEach(function (v) v.parentNode.appendChild(v) ) };
-		console.log(this.rows.length + " commands");
-		
-		for (var header in this.headers) {
-			var headerObj = this.headers[header];
-			var headerEl = $xs(headerObj.xp);
-			headerEl.addEventListener('click', curry(this.sortByHeader, this, header), true);
-		}
-	},
-	
-	sortByHeader: function(header) {
-		console.log("sort by header:" + header);
-		tictac.tic();
-		var headerObj = this.headers[header];
-		headerObj.sortby = true;
-		
-		this.rows.sort(function (rowa, rowb) {
-			var texta = $xs(headerObj.cell_xp, rowa).textContent, textb = $xs(headerObj.cell_xp, rowb).textContent;
-			return headerObj.sort(texta, textb)
-		});
-		
-		tictac.tic();
-		this.rows.reinsert();
-		console.log("reinserting took: " + tictac.tac());
-		console.log(sprintf("sorting took: \f millis", tictac.tac()));
-	}
-}
+  setListeners: function() {
+    this.rows = $x(this.unstable.rowxp);
+    this.rows.reinsert = function () { this.forEach(function (v) { v.parentNode.appendChild(v); } ); };
+    console.log(this.rows.length + " commands");
+
+    for (var header in this.headers) {
+      var headerObj = this.headers[header];
+      var headerEl = $xs(headerObj.xp);
+      headerEl.addEventListener('click', curry(this.sortByHeader, this, header), true);
+    }
+  },
+
+  sortByHeader: function(header) {
+    console.log("sort by header:" + header);
+//    tictac.tic();
+    var headerObj = this.headers[header];
+    headerObj.sortby = true;
+
+    this.rows.sort(function (rowa, rowb) {
+		     var texta = $xs(headerObj.cell_xp, rowa).textContent,
+		     textb = $xs(headerObj.cell_xp, rowb).textContent;
+		     return headerObj.sort(texta, textb);
+		   });
+
+    tictac.tic();
+    this.rows.reinsert();
+/*    console.log("reinserting took: " + tictac.tac());
+    console.log(sprintf("sorting took: \f millis", tictac.tac()));*/
+  }
+};
 
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
-herdSorter.setListeners();
-
-/*rows = $x(volatile.rowxp)
-	.map(function(v) { 
-		v.subs = parseInt($xs(volatile.cellxp, v).textContent, 10);
-		return v;
-	})
-	.sort(function (a,b) { return b.subs - a.subs; })
-	.forEach(function (v) { v.parentNode.appendChild(v); });
-*/	
-
+profile(herdSorter).setListeners();
 
 /* std functions */
 
 var tictac = {
-	now: function () {
-		return new Date();
-        },
-	tic: function() {
-        	this.times = this.times || [];
-		return this.times.push(new Date());
-	},
-	tac: function() {
-        	this.times = this.times || [];
-		return this.now() - this.times.pop();
-        }
+  now: function () {
+    return new Date();
+  },
+  tic: function() {
+    this.times = this.times || [];
+    return this.times.push(new Date());
+  },
+  tac: function() {
+    this.times = this.times || [];
+    return this.now() - this.times.pop();
+  }
 };
 
+function profile(obj){
+  var tictac = {
+    now: function () {
+      return new Date();
+    },
+    tic: function() {
+      this.times = this.times || [];
+      return this.times.push(new Date());
+    },
+    tac: function() {
+      this.times = this.times || [];
+      return this.now() - this.times.pop();
+    }
+  };
+
+  for (var prop in obj) {
+    if (obj[prop].constructor == Function) {
+      console.log("adding profiling for " + prop);
+      obj["_" + prop] = obj[prop];
+      obj[prop] = (function(prop) {
+		     return function () {
+		       console.log("profiling " + prop);
+		       tictac.tic();
+		       console.log($A(arguments).join(", "));
+		       obj["_" + prop].apply(obj, arguments);
+		       console.log(prop + ": " + tictac.tac() + " ms");
+		     };
+		   })(prop);
+    }
+  }
+  return obj;
+}
+
 function parseRelDate(relDateStr) {
-	var dateParts = relDateStr.replace("ago", "").trim().split(","), now = (new Date());
-	for (var i = 0; i < dateParts.length; i++) {
-		var m = dateParts[i].trim().split(/\W+/);
-		var value = m[0], unit = m[1];
-		if (unit.match(/weeks?/)) {
-			now.setDate(now.getDate() - value*7);
-		}
-		else if (unit.match(/days?/)) {
-			now.setDate(now.getDate() - value);
-		}
-		else if (unit.match(/hours?/)) {
-			now.setHours(now.getHours() - value);
-		}
-		else if (unit.match(/minutes?/)) {
-			now.setMinutes(now.getMinutes() - value);
-		}
-	}
-	return now;
+  var dateParts = relDateStr.replace("ago", "").trim().split(","), now = (new Date());
+  for (var i = 0; i < dateParts.length; i++) {
+    var m = dateParts[i].trim().split(/\W+/);
+    var value = m[0], unit = m[1];
+    if (unit.match(/weeks?/)) {
+      now.setDate(now.getDate() - value*7);
+    }
+    else if (unit.match(/days?/)) {
+      now.setDate(now.getDate() - value);
+    }
+    else if (unit.match(/hours?/)) {
+      now.setHours(now.getHours() - value);
+    }
+    else if (unit.match(/minutes?/)) {
+      now.setMinutes(now.getMinutes() - value);
+    }
+  }
+  return now;
 }
 
 
 
 function parseIntSort(a,b,asc) {
-	return sortf(parseInt(a,10), parseInt(b,10), asc);
+  return sortf(parseInt(a,10), parseInt(b,10), asc);
 }
 
-function sortf(a, b, asc) {	
-	if (asc)
-		return (a-b);
-	else
-		return -sortf(a,b, true);
+function sortf(a, b, asc) {
+  if (asc)
+    return (a-b);
+  else
+    return -sortf(a,b, true);
 }
 
 function sortstr(a, b, asc) {
-	if (asc)
-		return (a > b) ? 1 : -1;
-	else
-		return !sortf(a, b, true);
+  if (asc)
+    return (a > b) ? 1 : -1;
+  else
+    return !sortf(a, b, true);
 }
 
-function dateSort(a, b, asc) sortf(parseRelDate(a), parseRelDate(b), asc);
+function dateSort(a, b, asc) { return sortf(parseRelDate(a), parseRelDate(b), asc); };
 
 function $x(xpath, root) { // From Johan Sundström
-	var doc = root ? root.evaluate ? root : root.ownerDocument : document, next;
-	var got = doc.evaluate(xpath, root||doc, null, null, null), result = [];
-	while(next = got.iterateNext())
-		result.push(next);
-	return result;
+  var doc = root ? root.evaluate ? root : root.ownerDocument : document, next;
+  var got = doc.evaluate(xpath, root||doc, null, null, null), result = [];
+  while((next = got.iterateNext())) {
+	  result.push(next);
+  }
+  return result;
 }
 
-function $xs(xpath, root) $x(xpath, root)[0];
-String.prototype.trim = function() this.replace(/^\W+|\W+$/gi, "");
+function $xs(xpath, root) { return $x(xpath, root)[0]; }
+String.prototype.trim = function() { return this.replace(/^\W+|\W+$/gi, ""); };
 
 function bind(method, obj) {
-	return function() {
-		method.apply(obj, arguments);
-	};
+  return function() {
+    method.apply(obj, arguments);
+  };
 }
 
 function curry(method, obj) {
-	var curryargs = $A(arguments).slice(2);
-	return function() method.apply(obj || window, curryargs.concat($A(arguments)));
+  var curryargs = $A(arguments).slice(2);
+  return function() { return method.apply(obj || window, curryargs.concat($A(arguments))); };
 }
 
 function $A(arr) {
-    var r = [], len = arr.length;
-    while (len--) r.unshift(arr[len])
-    return r;
+  var r = [], len = arr.length;
+  while (len--) r.unshift(arr[len]);
+  return r;
 }
 
 function sprintf(str) {
-	var a;
-	args = $A(arguments);
-	args.shift();
-	while ((a = args.shift()) !== undefined) 
-		str = str.replace("\f", a);
-	return str;
+  var a;
+  args = $A(arguments);
+  args.shift();
+  while ((a = args.shift()) !== undefined)
+    str = str.replace("\f", a);
+  return str;
 }
